18f47332f1ebbcecc4a30731c2737f685d903d58
Joyce Zhang <joycezhang2065@gmail.com>
2023-02-26 16:44:04 -0500

    added homepage text

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,8 +3,7 @@
 function Homepage() {
   return (
     <div className="content">
-      This is the landing page for the site. 
-      <h1 style={{fontFamily: "SpongeboyRegular", color: "blue"}}>Welcome to Bikini Bottom!</h1>
+      <h1>Explore Canada Today</h1>
     </div>
   );
 }
